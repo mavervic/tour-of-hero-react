d4b10ae116be56e9bf2344acc8c450d5e62dbf02
Vic Liu <vic.liu@webcomm.com.tw>
2023-07-27 18:51:39 +0800

    feat: tic tac 初版

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Board from './TicTacToe/TicTacToeApp';
 import './index.css';
-import QuickStartApp from './quick-start/QuickStartApp.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <App /> */}
-    <QuickStartApp />
+    {/* <QuickStartApp /> */}
+    <Board />
   </React.StrictMode>
 );
