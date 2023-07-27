0e005af22a9347bfed8d3a8b83f46757ce2cd44c
Vic Liu <vic.liu@webcomm.com.tw>
2023-07-27 16:03:02 +0800

    feat: use env

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <h1>{import.meta.env.VITE_APP_TITLE}</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
