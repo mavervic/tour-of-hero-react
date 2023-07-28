2c28988226a7920b807f63e2d7bd83363b5b4709
Vic Liu <vic.liu@webcomm.com.tw>
2023-07-28 14:17:09 +0800

    feat: tic tac toe

--- src/TicTacToe/TicTacToeApp.tsx
+++ src/TicTacToe/TicTacToeApp.tsx
@@ -5,6 +5,12 @@ type SquareProps = {
   onSquareClick: () => void;
 };
 
+type BoardProps = {
+  xIsNext: boolean;
+  squares: Array<string>;
+  onPlay: (nextSquares: Array<string>) => void;
+};
+
 function Square({ value, onSquareClick }: SquareProps) {
   return (
     <button className="square" onClick={onSquareClick}>
@@ -13,44 +19,15 @@ function Square({ value, onSquareClick }: SquareProps) {
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState<Array<string>>(Array(9).fill(''));
-
+function Board({ xIsNext, squares, onPlay }: BoardProps) {
   function handleClick(idx: number) {
     if (squares[idx] || calculateWinner(squares)) {
       return;
     }
 
-    const newSquares = squares.slice();
-    newSquares[idx] = xIsNext ? 'X' : 'O';
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
-  }
-
-  function calculateWinner(squares: Array<string>): string {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-
-    return '';
+    const nextSquares = squares.slice();
+    nextSquares[idx] = xIsNext ? 'X' : 'O';
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -81,4 +58,69 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState<Array<Array<string>>>([
+    Array(9).fill(''),
+  ]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares: Array<string>) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove: number) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((_, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares: Array<string>): string {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return '';
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Board from './TicTacToe/TicTacToeApp';
+import Game from './TicTacToe/TicTacToeApp';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <QuickStartApp /> */}
-    <Board />
+    <Game />
   </React.StrictMode>
 );
