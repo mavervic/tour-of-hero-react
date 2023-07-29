82f369c08205e37842e41750751cf2fa8b519047
Vic.Liu <sunset1117@hotmail.com>
2023-07-29 14:23:37 +0800

    style: css modules .root testing

--- src/css-modules-demo/MyComponent.tsx
+++ src/css-modules-demo/MyComponent.tsx
@@ -3,12 +3,12 @@ import styles from './MyComponent.module.css';
 
 const MyComponent = () => {
   return (
-    <>
+    <div className={styles.root}>
       <div className={styles.container}>
         <h1 className={styles.red}>Hello, world!</h1>
         <h1 className={styles.myClass}>MY Class</h1>
       </div>
-    </>
+    </div>
   );
 };
 
