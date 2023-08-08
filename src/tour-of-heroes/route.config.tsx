bdf342195e20be0a33ccf0ba0051c2e812063057
Vic Liu <sunset1117@hotmail.com>
2023-08-08 21:04:45 +0800

    fix: lazy loading bugs

--- src/tour-of-heroes/route.config.tsx
+++ src/tour-of-heroes/route.config.tsx
@@ -1,10 +1,13 @@
-import { lazy } from 'react';
 import { Navigate, createBrowserRouter } from 'react-router-dom';
 import TourOfHeroesApp from './TourOfHeroesApp';
+import Dashboard from './components/dashboard/Dashboard';
+import HeroDetail from './components/hero-detail/HeroDetail';
+import Heroes from './components/heroes/Heroes';
 
-const Dashboard = lazy(() => import('./components/dashboard/Dashboard'));
-const HeroDetail = lazy(() => import('./components/hero-detail/HeroDetail'));
-const Heroes = lazy(() => import('./components/heroes/Heroes'));
+// TODO: lazy loading
+// const Dashboard = lazy(() => import('./components/dashboard/Dashboard'));
+// const HeroDetail = lazy(() => import('./components/hero-detail/HeroDetail'));
+// const Heroes = lazy(() => import('./components/heroes/Heroes'));
 
 const TOUR_OF_HERO_ROUTER = createBrowserRouter([
   {
